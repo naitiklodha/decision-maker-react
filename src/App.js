58602eddc6823b467fcdc079942117be515ccbae
naitik-lodha <naitik.lodha139@nmims.edu.in>
2023-02-17 20:51:26 +0530

    Added new features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import { MdDeleteOutline } from "react-icons/md";
+import Typewriter from "typewriter-effect";
 
 function App() {
   const [options, setOptions] = useState([]);
   const [optInput, setOptInput] = useState("");
   const [decision, setDecision] = useState("");
 
-  const btnStyle="p-8 py-4 rounded-lg  bg-dm-neon-blue hover:bg-purple-500  text-dm-black md:ml-4 md:py-2";
+  const btnStyle =
+    "p-4 py-2 rounded-lg  bg-dm-neon-blue hover:bg-pink-500  text-dm-black md:ml-4 md:px-8";
 
   function handleAdd() {
     if (optInput !== "" && optInput !== " " && optInput !== null) {
@@ -14,6 +17,12 @@ function App() {
       setOptInput("");
     }
   }
+  function handleRemoveOption(index) {
+    const newList = [...options];
+    newList.splice(index, 1);
+    setOptions(newList);
+  }
+
   function handleChange(e) {
     setOptInput(e.target.value);
   }
@@ -25,11 +34,17 @@ function App() {
     setDecision("");
     setOptions([]);
   }
+  function handleTryWithPreviousOptions() {
+    if (options.length > 0) {
+      handleDecision();
+    } else {
+      handleTry();
+    }
+  }
   function handleKeyDown(e) {
     if (e.key === "Enter") {
       handleAdd();
     }
-
   }
   return (
     <div className="flex flex-col items-center bg-dm-yellow font-roboto h-screen">
@@ -39,11 +54,7 @@ function App() {
         </h1>
 
         {options.length > 1 && decision === "" ? (
-          <button
-            className={btnStyle}
-            type="button"
-            onClick={handleDecision}
-          >
+          <button className={btnStyle} type="button" onClick={handleDecision}>
             Make Decision
           </button>
         ) : (
@@ -51,9 +62,15 @@ function App() {
         )}
 
         {decision ? (
-          <h1 className=" text-5xl animate-pulse mt-10 text-pink-500">
-            {decision}{" "}
-          </h1>
+          <div className=" text-5xl mt-10 text-dm-neon-blue font-bold">
+            <Typewriter
+              options={{
+                strings: decision,
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
         ) : (
           <br />
         )}
@@ -68,11 +85,7 @@ function App() {
               onKeyDown={handleKeyDown}
               className="p-2 rounded-md text-black border-2 mb-4 bg-transparent  border-black focus:outline-none md:mb-0 placeholder:text-gray-800"
             ></input>
-            <button
-              type="button"
-              className={btnStyle}
-              onClick={handleAdd}
-            >
+            <button type="button" className={btnStyle} onClick={handleAdd}>
               Add
             </button>
           </div>
@@ -81,34 +94,60 @@ function App() {
         )}
 
         {options.length > 0 ? (
-          <h2 className="m-4 text-dm-neon-blue font-bold font-roboto text-xl">
-            Options added till now:{" "}
-          </h2>
-        ) : (
-          <></>
-        )}
-        <div className="flex  items-center mb-8 flex-col">
-          {options.map((option, index) => (
-            <li className="text-lg list-none text-black" key={option + index}>
-              {option}
-            </li>
-          ))}
-        </div>
+          <div>
+            <h2 className="m-4 text-pink-500 font-bold font-roboto text-xl">
+              Options added till now:
+            </h2>
+            <ul className="mb-8">
+              {options.map((option, index) => (
+                <li
+                  key={option + index}
+                  className="flex justify-between items-center"
+                >
+                  <span className="text-lg list-none text-black">{option}</span>
+                  <button
+                    type="button"
+                    className="px-3 mr-2 text-red-500"
+                    onClick={() => handleRemoveOption(index)}
+                  >
+                    <MdDeleteOutline size={25}></MdDeleteOutline>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+
         {decision ? (
-          <button
-            type="button"
-            className={btnStyle}
-            onClick={handleTry}
-          >
-            Try Again
-          </button>
+          <div className="flex flex-col items-center">
+            <button
+              type="button"
+              className={btnStyle}
+              onClick={handleTryWithPreviousOptions}
+            >
+              Try Again
+            </button>
+            <div
+              className="mt-5 underline  cursor-pointer hover:text-pink-500"
+              onClick={handleTry}
+            >
+              Try again with different options
+            </div>
+          </div>
         ) : (
           <br></br>
         )}
       </div>
       <footer className="flex flex-col items-center justify-center h-16 bg-dm-neon-blue bottom-0 fixed w-screen text-dm-black font-roboto">
-
-        <h1>Developed by <a href="https://github.com/naitik-lodha" className="hover:text-dm-yellow">Naitik Lodha</a></h1>
+        <h1>
+          Developed with <span className="animate-pulse">❤️</span> by{" "}
+          <a
+            href="https://github.com/naitik-lodha"
+            className="text-pink-700 font-bold italic underline hover:text-yellow-500"
+          >
+            Naitik Lodha
+          </a>
+        </h1>
       </footer>
     </div>
   );
